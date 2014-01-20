d408372e45b9097206133f8d491c330c4f8fbd1b
Danny <dlopes690@gmail.com>
2014-01-20 01:54:11 -0500

    Created Show Post page

--- app/views/posts/show.blade.php
+++ app/views/posts/show.blade.php
@@ -1,1 +1,47 @@
-show.blade
+@extends('layouts.master')
+
+@section('content')
+
+<h1>Show Post</h1>
+
+<p>{{ link_to_route('posts.index', 'Return to all Posts') }}</p>
+	<div class="span4 well">
+		<table class="table table-striped table-bordered table-hover">
+			<thead>
+				<tr>
+					<th>Author</th>
+					<th>Title</th>
+					<th>Body</th>
+				</tr>
+			</thead>
+
+			<tbody>
+				<tr>
+					<td>{{ $post->author }}</td>
+					<td>{{ $post->title }}</td>
+					<td>{{ $post->body }}</td>
+					<td>{{ link_to_route('posts.edit', 'Edit', array($post->id), array('class' => 'btn btn-info')) }}</dr>
+					<td>
+						{{ Form::open(array('method' => 'DELETE', 'route' => array('posts.destroy', $post->id))) }}
+						{{ Form::submit('Delete', array('class' => 'btn btn-danger')) }}
+						{{ Form::close() }}
+					</td>
+				</tr>
+
+			</tbody>
+		</table>
+
+		
+		
+
+			<!-- if there are any errors, print them out -->
+		@if($errors->any())
+			<ul>
+				{{ implode('', $errors->all('<li class="error">:message</li>')) }}
+			</ul>
+		@endif
+
+	</div>
+
+
+@stop
