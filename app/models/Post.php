1a766ee670b2807f511a80eef75cb5cdc3ffea8d
Danny <dlopes690@gmail.com>
2014-01-20 00:23:16 -0500

    Added Posts Table, Files, Removed Code
    Removed Code from routes.php and left only the function that loads the
    index
    
    Added pages associated with Posts table through artisan in the command
    line

--- app/models/Post.php
+++ app/models/Post.php
@@ -1,13 +1,7 @@
 <?php
 
-class Post extends Eloquent
-{
-	public function User()
-	{
-			// if there would be a foreign key different to user_id, declare it as second parameter like in this case which is author_id
-		return belongsTo('user', 'author_id');
-	}
+class Post extends Eloquent {
+	protected $guarded = array();
 
-
-
+	public static $rules = array();
 }
--- app/routes.php
+++ app/routes.php
@@ -15,78 +15,6 @@
 // These routes link and load the pages
 Route::get('/', function()
 {
-		// different ways to perform sql queries, either we can do normal SQL or we can use Laravel built in functions
-	//$user = DB::table('employees')->get();
-	//$user = DB::table('employees')->where('country', 'United Kingdom')->get();
-	//$user = DB::table('employees')->whereBetween('employeeid', array('3', '5'))->get();
-	// $user = DB::table('employees')->orderBy('lastName', 'asc')->get();
-	//$user = DB::table('employees')->select('firstname', 'country')->get();
-	//$user = DB::table('employees')->where('employeeid','<', '4')->orWhere('country','United Kingdom')->get();
-	//$user = DB::select('SELECT * FROM employees WHERE country = ?', array('United Kingdom'));
-
-	// foreach($user as $u)
-	// {
-	// 	echo $u->LastName.'<br>';
-	// }
-
-	// echo '<pre>';
-	// dd($user);
-	// echo '</pre>';
-
-	$users = User::all();
-
-	$title = 'Home';
-	return View::make('home/index')->with('users', $users)
-			->with('title',$title);
-});
-
-Route::get('about', function()
-{
-	$title = 'About';
-	return View::make('home/about')
-			->with('title', $title);
-});
-
-	// post it with a name, if we post it using just '/' it will not work
-// right now it takes the values from the form and inserts it into the test
-// table in the database
-Route::post('test', function()
-{
-	$input = Input::all();
-
-		// creates the validator which is required to perform validation
-	$v = Validator::make($input, User::$rules, User::$messages);
-
-		// if validation passes, go ahead and insert name into table
-		// then redirect to the homepage
-	if($v->passes())
-	{
-			// inserting into table using Eloquent and model
-		$user = new User;
-		$user->fname = $input['fname'];
-		$user->lname = $input['lname'];
-
-		$user->save();
-		// DB::table('test')->insert(array(
-		// 	'fname' => $input['fname'], 
-		// 	'lname' => $input['lname']
-		// ));
-
-		return Redirect::to('/');
-	}
-		// if it fails, show the error messages and load create page again
-	else
-	{
-		return Redirect::to('create')
-			->withInput()
-			->withErrors($v)
-			->with('message');
-	}
-
-		// laravel way of inserting data to table
-	
-	
-	//DB::insert('INSERT INTO test (fname, lname) VALUES (?, ?)', array($input['fname'], $input['lname']));
 
 
 	$title = 'Home';
@@ -94,9 +22,7 @@ Route::post('test', function()
 			->with('title',$title);
 });
 
-Route::get('create', function()
-{
-	$title = 'Add New User';
-	return View::make('home/create')
-			->with('title',$title);
-});
+
+
+Route::resource('posts', 'PostsController');
+
