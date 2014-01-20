0b4e9407dae49eb0535ef5c6e5e8995394f815d6
Danny <dlopes690@gmail.com>
2014-01-20 00:58:24 -0500

    Created Master page for Posts

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -5,12 +5,46 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+	<link href="//netdna.bootstrap.cdn.com/twitter-bootstrap/2.3.1/css/bootstrap-combined.min.css" rel="stylesheet">
+
+	<style type="text/css">
+		table form{
+			margin-bottom: 0;
+		}
+
+		form ul{
+			margin-left: 0;
+			list-style: none;
+		}
+
+		.error{
+			color:red;
+			font-style: italic;
+		}
+
+		body{
+			padding-top: 20px;
+		}
+
+	</style>
+
 	<title>{{ $title }}</title>
 	{{ HTML::style('css/style.css') }}
 </head>
 <body>
 	@include('layouts/nav')
-	@yield('content')
+
+	<div class="container">
+		@if(Session::has('message'))
+			<div class="flash alert">
+				<p>{{ Session::get('message') }} </p>
+			</div>
+		@endif
+
+		@yield('content')
+	</div>
+
+	
 
 </body>
 </html>
